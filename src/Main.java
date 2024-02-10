afe55028617b45f51f1833635ea1bf5244473238
user <89169555954@mail.ru>
2024-02-11 00:34:47 +0300

    Makaronchik9/job4j_elementary

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello");
+        System.out.println(10*2);
+        System.out.println(2-3);
+        System.out.println(5/1);
+        System.out.println(10+1);
+        }
+
+}
