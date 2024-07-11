c25c71e32bf22b20cd7d9061c35d1694233a7890
Konstantin <89169555954@mail.ru>
2024-07-11 23:07:41 +0300

    Задание по булиан

--- src/Vetvlenie/Ycheba.java
+++ src/Vetvlenie/Ycheba.java
@@ -1,6 +1,20 @@
 package Vetvlenie;
 
 public class Ycheba {
-    public static void main(String[] args) {
+    public static void TruD (int number ) {
+
+        if (number == 0) {
+            System.out.println("Could not div by 0.");
+        } else if (number < 0)
+        System.out.println("This is negative number");
+
+    }
+
+   public static void main(String[] args) {
+    Ycheba.TruD (-1);
+    Ycheba.TruD(4);
+    Ycheba.TruD(0);
     }
 }
+
+
