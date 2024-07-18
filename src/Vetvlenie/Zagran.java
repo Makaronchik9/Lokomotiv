0727c502dc8a48410b540f22cd8f548d03777351
Konstantin <89169555954@mail.ru>
2024-07-18 23:03:22 +0300

    Загран c

--- src/Vetvlenie/Zagran.java
+++ src/Vetvlenie/Zagran.java
@@ -2,15 +2,16 @@ package Vetvlenie;
 
 public class Zagran {
     public static void Poezdka (boolean passport) {
-        System.out.println("У клиента есть загранник");
-        if (passport) {
-            System.out.println("Полетели зарубеж");
-        } else {
-            System.out.println("Путешествуем по России");
-        }
 
+        System.out.println("A client has foreign passport : " + passport);
+        if (passport) {
+            System.out.println("Tours abroad.");
+        } else {
+            System.out.println("Tours in Russia.");
+        }
     }
-    public static void main(String[] args) {
+
+            public static void main(String[] args) {
         Zagran.Poezdka(true);
         Zagran.Poezdka(false);
     }
